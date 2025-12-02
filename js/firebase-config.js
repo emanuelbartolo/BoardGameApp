70067ccb46acb9b2ef9ac1c960b85f08416ca6a5
Emanuel Bartolo <emanuelbartolo@gmail.com>
2025-12-02 21:43:23 +0100

    Add real Firebase config (apiKey redacted in commit msg)

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,14 +3,14 @@
 // Consider using Firebase Hosting environment variables to store this.
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "xxx",
+  authDomain: "boardgameapp-cc741.firebaseapp.com",
+  projectId: "boardgameapp-cc741",
+  storageBucket: "boardgameapp-cc741.firebasestorage.app",
+  messagingSenderId: "482372219202",
+  appId: "1:482372219202:web:a4598f340ea5e3751bd8db"
 };
 
 // Initialize Firebase
-// const app = firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore();
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
